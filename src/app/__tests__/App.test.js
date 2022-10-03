f106b50bbc7197622b5db272384b151f78f33d79
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-10-03 17:53:45 -0500

    fix linters

--- src/app/__tests__/App.test.js
+++ src/app/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from '../../../redux/configStore';
+import store from '../../redux/configStore';
 import App from '../App';
 
 test('renders learn react link', () => {
